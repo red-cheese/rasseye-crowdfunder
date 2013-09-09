c39a863a640767b102d4ebc3019ff14399fe990f
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2013-09-10 01:34:38 +0400

    RASSEYE! crowdfunder 1.0 ready

---
+++ web.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var fs = require('fs');
+
+
+// Configuration
+
+var app = express.createServer(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+// Render html pages
+app.set('view options', { layout: false });
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/');
+// Serve static files like images
+app.use(express.static(__dirname + '/public'));
+
+
+// Routes
+
+app.get('/', function(req, res) {
+	res.render('index.ejs');
+});
+
+app.get('/us', function(req, res) {
+	res.render('us.ejs');
+});
+
+app.get('/roadmap', function(req, res) {
+	res.render('roadmap.ejs');
+});
+
+app.get('/blog', function(req, res) {
+	res.render('blog.ejs');
+});
+
+app.get('/fame', function(req, res) {
+	res.send("Our future wall of fame, your name can be here!".toString('utf-8'));
+});
+
+app.get('/btc', function(req, res) {
+	res.render('btc.ejs');
+});
+
+
+// Port
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
